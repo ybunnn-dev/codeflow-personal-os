c3aac03cdc8efbcb2bb33b36434fc2d4028c99e9
John Ivan Belaro <belaroivan17@gmail.com>
2026-08-06 23:33:25 +0800

    Updated profile section

--- app/(view)/(panel)/hire_me/profile/components/personnal_details.tsx
+++ app/(view)/(panel)/hire_me/profile/components/personnal_details.tsx
@@ -8,7 +8,11 @@ export default function PersonalDetails() {
         lastName: '',
         firstName: 'John Ivan',
         middleName: 'Balaoro',
-        suffix: 'JR'
+        suffix: 'JR',
+        address: 'Irosin, Sorsogon',
+        mobileNo: '09672474019',
+        email: 'belaroivan17@gmail.com',
+        portfolio: 'https://ybunnn-my-portfolio.vercel.app/'
     });
 
    
@@ -25,7 +29,7 @@ export default function PersonalDetails() {
                 </button>
             </div>
 
-            <div className="bg-white dark:bg-slate-900 w-full p-6 lg:p-10 rounded-xl border border-transparent dark:border-slate-700/50 shadow-sm transition-colors">
+            <div className="bg-white flex flex-col gap-6 dark:bg-slate-900 w-full p-6 lg:p-10 rounded-xl border border-transparent dark:border-slate-700/50 shadow-sm transition-colors">
                 <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 lg:gap-4">
                     
                     <div className="flex flex-col">
@@ -71,6 +75,59 @@ export default function PersonalDetails() {
                           
                             className={inputClasses} 
                         />
+                    </div>
+
+                </div>
+
+                <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 lg:gap-4">
+                    
+                    <div className="flex flex-col">
+                        <label className={labelClasses}>Mobile Number</label>
+                        <input 
+                            type="text" 
+                            name="mobileNo"
+                            value={details.mobileNo}
+                            
+                            className={inputClasses} 
+                            placeholder="Enter last name"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className={labelClasses}>Email</label>
+                        <input 
+                            type="text" 
+                            name="email"
+                            value={details.email}
+                            
+                            className={inputClasses} 
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className={labelClasses}>Address</label>
+                        <input 
+                            type="text" 
+                            name="address"
+                            value={details.address}
+                            
+                            className={inputClasses} 
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label className={labelClasses}>Portfolio Link</label>
+                        <input 
+                            type="text" 
+                            name="portfolio_link"
+                            value={details.portfolio}
+                          
+                            className={inputClasses} 
+                        />
+                    </div>
+
+                    <div className=''>
+
                     </div>
 
                 </div>
